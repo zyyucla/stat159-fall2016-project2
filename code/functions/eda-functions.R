45b60cb0febd5bf133cf0851a2075ace68211dfb
Yuyu Zhang <zyyucla@gmail.com>
2016-10-22 10:05:53 -0700

    script: added the xtable for the script function

--- code/functions/eda-functions.R
+++ code/functions/eda-functions.R
@@ -1,13 +1,15 @@
 #description: computes the required summary for quantitative variables
-#input: a quantative variable from "Credit.csv" data set
+#input: the object from  "Credit.csv" set
 #output: a summary statistics set of the variable
 
 quant-summary<-function(x){
+   librar(xtable)
+   for (i in 2:7){
+   var-name<-names(x)[i]
    stats<-data.frame(Quantity=c("Min: ", "Max: ", "Range: ", "Median: ", "Mean: ",
-"1st Qu: ", "3rd Qu: ", "IQR: ", "STD: "), Value=c(min(x), max(x), max(x)-min(x), median(x),
- mean(x), quantile(x,0.25), quantile(x,0.75), IQR(x), sd(x)) )
-  
-  
-  
-
+"1st Qu: ", "3rd Qu: ", "IQR: ", "STD: "), Value=c(min(x$var-name), max(x$var-name), max(x$var-name)-min(x$var-name), median(x$var-name),
+ mean(x$var-name), quantile(x$var-name,0.25), quantile(x$var-name,0.75), IQR(x$var-name), sd(x$var-name)) )
+   stats-table<-xtable(stats,caption=var-name,digits=4,floating=FALSE)
+   print(stats-table,caption.placement="top")
+  }
 }

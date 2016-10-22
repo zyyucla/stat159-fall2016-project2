7a935732759b5bdf7398893850f54824fb83a0dd
Yuyu Zhang <zyyucla@gmail.com>
2016-10-22 13:01:47 -0700

    script: add the boxplot portion of the scripts

---
+++ code/scripts/eda-scripts-qualitative.R
@@ -0,0 +1,37 @@
+#Read data
+source("../functions/eda-script-frequency.R")
+dat<-read.csv("../data/Credit.csv",row.names=1)
+summary(dat)
+names(dat)
+str(dat)
+
+#Get qualitative subset of data
+sub-data<-dat[,8:11]
+#Output frequencies result to file
+sink("../../data/eda-output-qualitative.txt")
+print("Frequencies statistics of Qualitative Variables")
+print("\n\n")
+qual-summary(sub-data)
+cat("\n\n)
+
+#Create anova
+print("Anova between Balance and other qualitative variables")
+print("\n\n")
+fit<-aov(Balance~Gender+Student+Married+Education, dat)
+print(fit)
+sink()
+
+#Conditional Boxplots between Balance and Gender
+png("../../images/conditional-boxplot-gender.png")
+boxplot(Balance~Gender,dat, main="Boxplot of Balance on Gender", col="lightblue")
+
+png("../../images/conditional-boxplot-student.png")
+boxplot(Balance~Student,dat, main="Boxplot of Balance on Student", col="lightblue")
+
+png("../../images/conditional-boxplot-married.png")
+boxplot(Balance~Married,dat, main="Boxplot of Balance on Married", col="lightblue")
+
+
+png("../../images/conditional-boxplot-ethnicity.png")
+boxplot(Balance~Ethnicity,dat, main="Boxplot of Balance on Ethnicity", col="lightblue")
+dev.off()

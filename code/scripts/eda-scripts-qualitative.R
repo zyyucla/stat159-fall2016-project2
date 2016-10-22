6f64e221cdffd55f85b622a5c7020d9bc4713343
Yuyu Zhang <zyyucla@gmail.com>
2016-10-22 13:49:42 -0700

    script: added the barchart code

--- code/scripts/eda-scripts-qualitative.R
+++ code/scripts/eda-scripts-qualitative.R
@@ -1,4 +1,5 @@
 #Read data
+library(lattice)
 source("../functions/eda-functions.R")
 dat<-read.csv("../data/Credit.csv",row.names=1)
 summary(dat)
@@ -13,13 +14,13 @@ print("Frequencies statistics of Qualitative Variables")
 print("\n\n")
 qual-summary(sub-data)
 cat("\n\n")
-
 #Create anova
 print("Anova between Balance and other qualitative variables")
 print("\n\n")
 fit<-aov(Balance~Gender+Student+Married+Education, dat)
 print(fit)
 sink()
+
 
 #Conditional Boxplots between Balance and Gender
 png("../../images/conditional-boxplot-gender.png")
@@ -34,4 +35,19 @@ boxplot(Balance~Married,dat, main="Boxplot of Balance on Married", col="lightblu
 
 png("../../images/conditional-boxplot-ethnicity.png")
 boxplot(Balance~Ethnicity,dat, main="Boxplot of Balance on Ethnicity", col="lightblue")
+
+
+
+#Barchart of frequency counts
+png("../../images/barchart-frequency-gender.png")
+barchart(Gender,dat, main="Frequency count of Gender",xlab="Gender",ylab="Counts")
+
+png("../../images/barchart-frequency-student.png")
+barchart(Student,dat, main="Frequency count of Student",xlab="Student",ylab="Counts")
+
+png("../../images/barchart-frequency-married.png")
+barchart(Married,dat, main="Frequency count of Married",xlab="Married",ylab="Counts")
+
+png("../../images/barchart-frequency-ethnicity.png")
+barchart(Ethnicity,dat, main="Frequency count of Ethnicity",xlab="Ethnicity",ylab="Counts")
 dev.off()

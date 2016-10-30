1164ce1f13ded7eb53eee526fda50e46496b46d7
Yuyu Zhang <zyyucla@gmail.com>
2016-10-30 14:04:13 -0700

    function: added {} in the function body

--- code/functions/eda-functions.R
+++ code/functions/eda-functions.R
@@ -1,31 +1,16 @@
-#description: computes the required summary for quantitative variables
-#input: a set of data
-#output: a summary statistics set of the variables
- library(xtable)
-quant-summary<-function(x){
-   for (i in 1:6){
-   var-name<-names(x)[i]
-   stats<-data.frame(Quantity=c("Min: ", "Max: ", "Range: ", "Median: ", "Mean: ",
-"1st Qu: ", "3rd Qu: ", "IQR: ", "STD: "), Value=c(min(x$var-name), max(x$var-name), max(x$var-name)-min(x$var-name), median(x$var-name),
- mean(x$var-name), quantile(x$var-name,0.25), quantile(x$var-name,0.75), IQR(x$var-name), sd(x$var-name)) )
-   stats-table<-xtable(stats,caption=var-name,digits=4,floating=FALSE)
-   print(stats-table,caption.placement="top")
-   print("\n\n")
-  }
-}
 
 #description: computes the frequency summary for quantitative variables
-#input: the object from  "Credit.csv" set
-#output: frequency summary table of all the qualitative variables
+#input: one qualitative variable from dataset "Credit.csv" set
+#output: frequency summary table of the variable
 
-freq-summary<function(x){
-  for(i in 1:4){
-    var-name<-names(x)[i]
-    Counts<-summary(sdata$var-name)
-    Rel-Frequency<-prop.table(Counts)
-    total-table<-xtable(rbind(Counts,Rel-Frequency),caption=var-name,digits=4,floating=FALSE)
-    print(total-table,caption.placement="top")
-    print("\n\n")
-  }   
-   
+freq_summary<-function(x){
+    library(xtable)
+    name<-deparse(substitute(x)) 
+    print(name)
+    cat("\n")
+   # stats<-data.frame(counts=c(summary(x)),rel_frequency=c(prop.table(counts)))
+    counts<-summary(x)
+    rel_frequency<-prop.table(counts)
+    stats<-data.frame(rel_frequency,counts)   
+    print.data.frame(stats)
 }

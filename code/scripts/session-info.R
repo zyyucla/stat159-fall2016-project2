8182cec9e995a1e8058a9e18662397cd94e68ecb
Yuyu Zhang <zyyucla@gmail.com>
2016-11-02 09:40:59 -0700

    script: added session info R file

---
+++ code/scripts/session-info.R
@@ -0,0 +1,11 @@
+# include all R packages in your project
+library(devtools)
+library(knitr)
+library(rmarkdown)
+library(xtable)
+library(ggplot2)
+sink("../../session-info.txt", append = TRUE)
+cat("Session Information\n\n")
+print(sessionInfo())
+devtools::session_info()
+sink()

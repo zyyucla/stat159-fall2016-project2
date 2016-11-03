02f45ae024b3bb27fe1037a3897873b387d5abce
Bret Hart <bret777@gmail.com>
2016-11-03 02:12:42 -0700

    Script: move model scripts to subdirectory.

--- code/scripts/OLS-creation.R
+++
@@ -1,9 +0,0 @@
-
-#OLS - Multiple Linear Regression Model
-
-attach(data.train)
-
-OLS <- lm(Balance~., data=data.train)
-OLS.summ <- summary(OLS)
-
-save(OLS, OLS.summ, file = "data/models/OLS.RData")

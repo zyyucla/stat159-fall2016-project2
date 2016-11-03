7747e8087523ec6adb02c8fa7dcc550a9857b64d
Bret Hart <bret777@gmail.com>
2016-11-03 04:28:38 -0700

    Ridge: Create Script, Select model, output data objects and plot

---
+++ code/scripts/models/ridge-creation.R
@@ -0,0 +1,19 @@
+
+#Ridge Regression Model fitting
+
+#install.packages("glmnet")
+require(glmnet)
+load("data/train-test-data.RData")
+attach(data.train)
+
+grid <- 10^seq(10, -2, length = 100)
+
+set.seed(155)
+ridge.cv <- cv.glmnet(model.matrix(Balance ~ ., data.train), Balance, alpha = 0, 
+          intercept = FALSE, standardize = FALSE, lambda = grid)
+
+#plot(ridge.cv)
+
+save(ridge.cv, file = "data/models/ridge.RData")
+
+
---
+++ code/scripts/models/ridge-selection.R
@@ -0,0 +1,41 @@
+
+#Ridge Regression Model Selection
+
+load("data/models/ridge.RData")
+
+png('images/models/ridge-plotted-lambdas.png')
+plot(ridge.cv)
+dev.off()
+
+attach(data.train)
+
+ridge.lamb <- glmnet(model.matrix(Balance ~ ., data.train),
+                     Balance, alpha = 0, intercept = FALSE, standardize = FALSE,
+                     lambda = ridge.cv$lambda.min)
+
+ridge.pred <- predict(ridge.lamb, 
+                      newx = model.matrix(data.test$Balance ~., data.test))
+
+#Tested to see if this slightly different method of prediction gave a substantially different
+#result. It gives very similar results but is slightly less precise.
+
+#set.seed(155)
+#ridge.reg <- glmnet(model.matrix(Balance ~ ., data.train), Balance, alpha = 0,
+#                    intercept = FALSE, standardize = FALSE, lambda = grid)
+
+#ridge.pred2 <- predict(ridge.reg, s = ridge.cv$lambda.min,
+#                       newx = model.matrix(Balance ~., data.test))
+
+#mean((ridge.pred2 - data.test$Balance)^2)
+
+ridge.mse <- mean((ridge.pred - data.test$Balance)^2)
+
+data <- read.csv("data/Scaled-credit.csv", header = TRUE)
+data <- data[,3:14]
+
+ridge.final <- glmnet(model.matrix(data$Balance ~ ., data),
+                     data$Balance, alpha = 0, intercept = FALSE, standardize = FALSE,
+                     lambda = ridge.cv$lambda.min)
+
+save(ridge.mse, ridge.final, file = "data/models/ridge-outputs.RData")
+
